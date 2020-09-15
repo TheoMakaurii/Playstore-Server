37d168adc05b90b31f17d48b45bbc8217e396b8b
Tim Mahoney <timmahoney2000@gmail.com>
2020-09-15 14:33:23 -0400

    playstore.js assignment 9/15/20

--- app.js
+++ app.js
@@ -8,14 +8,28 @@ const apps= require('./playstore-data');
 
 
 app.get('/apps', (req, res)=>{
-  const {search =''} = req.query;
+  const {search ='', sort} = req.query;
+
+  if (sort) {
+    if (!['App', 'Rating'].includes(sort)) {
+      return res
+      .status(400)
+      .send('Sort must be one of app or rating');
+    }
+  }
 
   let results= apps
     .filter(playstore=>
       playstore
-        .Category
+        .Price
         .toLowerCase()
         .includes(search.toLowerCase()));
+
+        if (sort){
+          results.sort((a, b)=> {
+            return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+          });
+        }
   res 
     .json(results);
 });
